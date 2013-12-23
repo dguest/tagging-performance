13f4af499616594d9e1636a2208437354acd517b
Dan Guest <dguest@cern.ch>
2013-12-22 21:25:12 -0500

    got basic jet loop working

--- src/stand-alone.cxx
+++ src/stand-alone.cxx
@@ -6,5 +6,14 @@
 int main(int narg, char* argv[]) { 
   std::vector<std::string> files = {"test.root"}; 
   TreeBuffer buffer(files); 
+  const int n_events = std::min(200, buffer.size()); 
+  for (int event = 0; event < n_events; event++) { 
+    buffer.getEntry(event); 
+    const int n_jets = buffer.jet_pt->size(); 
+    for (int jidx = 0; jidx < n_jets; jidx++) { 
+      printf("pt: %f\n", buffer.jet_pt->at(jidx)); 
+    }
+  }
+
   return 0; 
 }
