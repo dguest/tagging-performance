567da753394d49b2930dfd45daae25c71434438c
Dan Guest <dguest@cern.ch>
2014-01-02 20:00:47 -0500

    work on ctagging contour plots

--- python/tagperf/ctag.py
+++ python/tagperf/ctag.py
@@ -3,7 +3,7 @@ import numpy as np
 import h5py
 import math
 import warnings
-import os
+import os, sys
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -19,56 +19,8 @@ def make_plots(in_file_name, cache_name, out_dir, ext):
         os.mkdir(out_dir)
 
     with h5py.File(cache_name, 'r') as cache: 
-        # print('slow')
-        # draw_ctag_rejrej_slow(cache, out_dir, ext)
         draw_ctag_rejrej(cache, out_dir, ext)
-
-def draw_ctag_rejrej_slow(in_file, out_dir, ext='.pdf'): 
-    fig = Figure(figsize=(8,6))
-    canvas = FigureCanvas(fig)
-    ax = fig.add_subplot(1,1,1)
-    ds = in_file['gaia/all']
-    xmin = ds.attrs.get('x_min', 1.0)
-    ymin = ds.attrs.get('y_min', 1.0)
-    xmax = ds.attrs['x_max']
-    ymax = ds.attrs['y_max']
-
-    xvals = np.logspace(math.log10(xmin), math.log10(xmax), ds.shape[0])
-    yvals = np.logspace(math.log10(ymin), math.log10(ymax), ds.shape[1])
-    xgrid, ygrid = np.meshgrid(xvals, yvals)
-
-    ax.pcolormesh(xgrid, ygrid, np.array(ds))
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    out_name = '{}/rejrej{}'.format(out_dir, ext)
-    canvas.print_figure(out_name, bbox_inches='tight')
-
-def draw_ctag_rejrej(in_file, out_dir, ext='.pdf'): 
-    fig = Figure(figsize=(8,6))
-    canvas = FigureCanvas(fig)
-    ax = fig.add_subplot(1,1,1)
-    ds = in_file['gaia/all']
-    xmin = ds.attrs.get('x_min', 1.0)
-    ymin = ds.attrs.get('y_min', 1.0)
-    xmax = ds.attrs['x_max']
-    ymax = ds.attrs['y_max']
-
-    eff_array = _maximize_efficiency(np.array(ds))
-    im = ax.imshow(eff_array.T, extent=(xmin, xmax, ymin, ymax), 
-              origin='lower', aspect='auto')
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.grid(which='both')
-
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    cb = Colorbar(ax=cax, mappable=im)
-
-    out_name = '{}/rejrej{}'.format(out_dir, ext)
-    # ignore complaints about not being able to scale images
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        canvas.print_figure(out_name, bbox_inches='tight')
+        draw_contour_rejrej(cache, out_dir, ext)
 
 def _get_hist_name(flavor, tagger, binning): 
     return '{}/ctag/{}/{}'.format(flavor, binning, tagger)
@@ -107,7 +59,20 @@ def make_rejrej(in_file, out_file, tagger='gaia', binning='all'):
     saved_ds = out_file[tagger].create_dataset(binning, data=rej_array)
     saved_ds.attrs['x_max'] = rej_builder.x_max
     saved_ds.attrs['y_max'] = rej_builder.y_max
+    saved_ds.attrs['xyz'] = 'BUC'
 
+class ProgBar: 
+    def __init__(self, total): 
+        self.total = total
+    def __enter__(self): 
+        return self
+    def __exit__(self, ex_type, ex_message, tr): 
+        sys.stdout.write('\n')
+    def update(self, entry): 
+        sys.stdout.write('\r{} of {} ({:.0%})'.format(
+                entry, self.total, entry / self.total))
+        sys.stdout.flush()
+    
 class RejRejComp: 
     def __init__(self): 
         self.n_bins = 100
@@ -124,12 +89,118 @@ class RejRejComp:
         z_vals = eff[valid_rej]
         
         out_array = np.ones((self.n_bins, self.n_bins)) * -1
-        for binn, (x, y, z) in enumerate(zip(x_bins, y_bins, z_vals)): 
-            if binn % 10000 == 0: 
-                print('going:', binn, x, y, z)
-            out_array[x, y] = max(z, out_array[x, y])
+        with ProgBar(z_vals.size) as pbar: 
+            for binn, (x, y, z) in enumerate(zip(x_bins, y_bins, z_vals)): 
+                if binn % 10000 == 0: 
+                    pbar.update(binn)
+                out_array[x, y] = max(z, out_array[x, y])
         return out_array
 
+
+# ============= drawing routines ===========
+
+def draw_ctag_rejrej_slow(in_file, out_dir, ext='.pdf'): 
+    """
+    Slow because it uses pcolormesh. 
+    """
+    fig = Figure(figsize=(8,6))
+    canvas = FigureCanvas(fig)
+    ax = fig.add_subplot(1,1,1)
+    ds = in_file['gaia/all']
+    xmin = ds.attrs.get('x_min', 1.0)
+    ymin = ds.attrs.get('y_min', 1.0)
+    xmax = ds.attrs['x_max']
+    ymax = ds.attrs['y_max']
+
+    xvals = np.logspace(math.log10(xmin), math.log10(xmax), ds.shape[0])
+    yvals = np.logspace(math.log10(ymin), math.log10(ymax), ds.shape[1])
+    xgrid, ygrid = np.meshgrid(xvals, yvals)
+
+    ax.pcolormesh(xgrid, ygrid, np.array(ds))
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    out_name = '{}/rejrej{}'.format(out_dir, ext)
+    canvas.print_figure(out_name, bbox_inches='tight')
+
+
+def draw_ctag_rejrej(in_file, out_dir, ext='.pdf'): 
+    fig = Figure(figsize=(8,6))
+    canvas = FigureCanvas(fig)
+    ax = fig.add_subplot(1,1,1)
+    ds = in_file['gaia/all']
+
+    eff_array, extent = _get_arr_extent(ds)
+    im = ax.imshow(eff_array.T, extent=extent, 
+                   origin='lower', aspect='auto')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.grid(which='both')
+    _label_axes(ax, ds)
+
+    # _add_contour(ax,ds)
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cb = Colorbar(ax=cax, mappable=im)
+
+    out_name = '{}/rejrej{}'.format(out_dir, ext)
+    # ignore complaints about not being able to log scale images
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        canvas.print_figure(out_name, bbox_inches='tight')
+
+def draw_contour_rejrej(in_file, out_dir, ext='.pdf'): 
+    fig = Figure(figsize=(8,6))
+    canvas = FigureCanvas(fig)
+    ax = fig.add_subplot(1,1,1)
+    ds = in_file['gaia/all']
+    
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.grid(which='both')
+    _add_contour(ax, ds)
+    _label_axes(ax, ds)
+
+    out_name = '{}/rejrej-cont{}'.format(out_dir, ext)
+    canvas.print_figure(out_name, bbox_inches='tight')
+
+# =========== draw utilities ===========
+
+def _add_contour(ax, ds, opts={}): 
+    eff_array = _maximize_efficiency(np.array(ds))
+    xmin = ds.attrs.get('x_min', 1.0)
+    ymin = ds.attrs.get('y_min', 1.0)
+    xmax = ds.attrs['x_max']
+    ymax = ds.attrs['y_max']
+
+    xvals = np.logspace(math.log10(xmin), math.log10(xmax), ds.shape[0])
+    yvals = np.logspace(math.log10(ymin), math.log10(ymax), ds.shape[1])
+    xgrid, ygrid = np.meshgrid(xvals, yvals)
+    contour_order, c_lines = _get_contour_order_and_lines([0,0.5])
+    ct = ax.contour(xgrid, ygrid, 
+        eff_array.T, 
+        linewidths = 2, 
+        levels = c_lines,
+        colors = opts.get('color','k'), 
+        )
+    import matplotlib.pyplot as plt
+    ax.clabel(ct, fontsize=12, inline=True, 
+              fmt = '%.{}f'.format(-contour_order + 1 ))
+
+def _label_axes(ax, ds): 
+    x, y, z = ds.attrs['xyz']
+    ax.set_xlabel('$1 / \epsilon_{{ {} }}$'.format(x.lower()), 
+                  x=0.98, ha='right')
+    ax.set_ylabel('$1 / \epsilon_{{ {} }}$'.format(y.lower()), 
+                  y=0.98, ha='right')
+
+def _get_arr_extent(ds): 
+    xmin = ds.attrs.get('x_min', 1.0)
+    ymin = ds.attrs.get('y_min', 1.0)
+    xmax = ds.attrs['x_max']
+    ymax = ds.attrs['y_max']
+    eff_array = _maximize_efficiency(np.array(ds))
+    return eff_array, (xmin, xmax, ymin, ymax)
 
 def _maximize_efficiency(eff_array): 
     """
@@ -138,3 +209,39 @@ def _maximize_efficiency(eff_array):
     temp = np.maximum.accumulate(eff_array[::-1,:], 0)[::-1,:]
     temp = np.maximum.accumulate(temp[:,::-1], 1)[:,::-1]
     return temp
+
+def _get_contour_order_and_lines(z_range): 
+    """
+    returns (order, lines), where order is an int, lines an array
+
+    we want somewhere between 6 and 15 contour lines in z_range, 
+    there's a messy way to do this, and it's not worth thinking about the 
+    cleaner way
+    """
+
+    z_min = min(z_range)
+    z_max = max(z_range)
+    contour_range = z_max - z_min
+    contour_order = math.trunc(math.log10(contour_range)) - 1
+    
+    c_min = round(z_min,-contour_order)
+    c_max = round(z_max,-contour_order)
+    round_range = c_max - c_min
+
+    base_increment = 10**contour_order
+    n_increments = round_range / base_increment
+    
+    if n_increments > 40: 
+        base_increment *= 5
+    elif n_increments > 15: 
+        base_increment *= 2
+    elif n_increments < 6: 
+        base_increment *= 0.5
+
+    n_increments = round_range / base_increment
+
+    the_lines = np.arange(c_min, c_max, base_increment)
+    
+    subset_in_range = (the_lines >= z_min) & (the_lines < z_max)
+    return contour_order, the_lines[ subset_in_range]
+
