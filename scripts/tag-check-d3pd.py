52e2d1f65cd2b9c4d7464a932cc006aef075ae71
Dan Guest <dguest@cern.ch>
2015-02-27 18:43:13 -0500

    some fiddling around with tag checking (found a pretty big drift in tagger efficiency)

--- scripts/tag-check-d3pd.py
+++ scripts/tag-check-d3pd.py
@@ -9,11 +9,13 @@ import h5py
 import numpy as np
 
 def get_args():
-    parser = argparse.ArgumentParser(description=__help__)
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('hdf_file')
     return parser.parse_args(sys.argv[1:])
 
 def _get_eff(dataset, cuts):
+    # ACHTUNG: the bounds should be read out of the dataset (this is
+    # a short-term hack because the DS isn't formated right)
     bounds = [(-10, 10)]*2
     n_bins = [x - 2 for x in dataset.shape]
     array = np.asarray(dataset)
@@ -28,7 +30,6 @@ def _get_eff(dataset, cuts):
     return passing / total
 
 def _check_tagger_eff(h5, tagger, cuts):
-    print(tagger)
     for flav in ['U','C','B']:
         eff = _get_eff(h5[flav]['ctag']['all'][tagger], cuts)
         print(flav, eff, 1/eff)
@@ -37,7 +38,13 @@ def _check_tagger_eff(h5, tagger, cuts):
 def run():
     args = get_args()
     with h5py.File(args.hdf_file, 'r') as h5:
+        print('old jfit OP')
         _check_tagger_eff(h5, 'jfit',  [-0.82, -1])
+        print('new jfit OP (moved to keep the same c-eff, and b-rej')
+        _check_tagger_eff(h5, 'jfit',  [-1.4, -0.9])
+        print('old jfc OP')
         _check_tagger_eff(h5, 'jfc', [0.95, -0.9])
+        print('new jfc OP (moved to keep the same c-eff, and b-rej')
+        _check_tagger_eff(h5, 'jfc', [0.42, -0.61])
 if __name__ == '__main__':
     run()
