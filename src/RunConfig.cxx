505b4d1b5eceea8c503803fb77611158a8f2e4f4
Dan Guest <dguest@cern.ch>
2015-11-20 15:11:08 +0100

    Fix compiler error with clang

--- src/RunConfig.cxx
+++ src/RunConfig.cxx
@@ -1,6 +1,8 @@
 #include "RunConfig.hh"
 #include "jtag.hh"
 
+#include <cstdlib>
+
 void usage(std::string call) {
   printf("usage: %s [-h] [-t] [-o <out file>] <root file>...\n",
 	 call.c_str());
@@ -27,7 +29,7 @@ RunConfig::RunConfig(int narg, char* argv[]):
 {
   if (narg == 1) {
     usage(argv[0]);
-    exit(1);
+    std::exit(1);
   }
   for (int pos = 1; pos < narg; pos++) {
     if (argv[pos][0] == '-') {
@@ -35,7 +37,7 @@ RunConfig::RunConfig(int narg, char* argv[]):
       if (strchr(argv[pos],'h')) {
 	usage(argv[0]);
 	help();
-	exit(1);
+	std::exit(1);
       }
       if (strchr(argv[pos], 'o')) {
 	if (pos + 1 < narg) {
@@ -43,7 +45,7 @@ RunConfig::RunConfig(int narg, char* argv[]):
 	  out_name = argv[pos];
 	} else {
 	  printf("error: -o must be followed by an output file name\n");
-	  exit(-1);
+	  std::exit(-1);
 	}
       }
     } else {
@@ -53,7 +55,7 @@ RunConfig::RunConfig(int narg, char* argv[]):
   if (files.size() == 0) {
     usage(argv[0]);
     printf("error: no files\n");
-    exit(-1);
+    std::exit(-1);
   }
 
 }
